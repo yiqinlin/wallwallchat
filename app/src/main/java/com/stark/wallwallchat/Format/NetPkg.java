5ada999f92e300ccfdc399c0e83d45335c1f32d4
Stark <yiqinlin@foxmail.com>
2017-08-29 13:09:11 +0800

    Merge branch 'master' of https://github.com/yiqinlin/yiyu
    # Conflicts:
    #	app/src/main/java/com/stark/yiyu/File/ImgStorage.java
    #	app/src/main/java/com/stark/yiyu/NetWork/NetPackage.java
    #	app/src/main/java/com/stark/yiyu/UIactivity/HomepageActivity.java

---
+++ app/src/main/java/com/stark/wallwallchat/Format/NetPkg.java
@@ -0,0 +1,18 @@
+package com.stark.wallwallchat.Format;
+
+import org.json.JSONArray;
+
+/**
+ * Created by Stark on 2017/8/13.
+ */
+public class NetPkg {
+    String type;
+    String from;
+    String to;
+    JSONArray S;
+    JSONArray I;
+    JSONArray L;
+    JSONArray F;
+    JSONArray D;
+    JSONArray B;
+}
